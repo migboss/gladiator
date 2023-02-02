701ccd31914c0c9ce4666d4f9d943976c02523bd
mig <114227605+migboss@users.noreply.github.com>
2023-02-02 15:12:38 +0000

    tutorial

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static(path.join(__dirname, 'public')));
+const usersRouter = require("./routes/usersRoutes");
+app.use("/api/users",usersRouter);
 const port = parseInt(process.env.port || '8080');
 app.listen(port,function() {
   console.log("Server running at http://localhost:"+port);
