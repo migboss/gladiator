88ec16ef20b3893aa4abf06931000ed16b08b9b6
mig <114227605+migboss@users.noreply.github.com>
2023-02-07 09:08:52 +0000

    Class 2

--- index.js
+++ index.js
@@ -11,9 +11,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.static(path.join(__dirname, 'public')));
+
 const usersRouter = require("./routes/usersRoutes");
+const cardsRouter = require("./routes/cardsRoutes");
+
 app.use("/api/users",usersRouter);
+app.use("/api/cards",cardsRouter);
+
 const port = parseInt(process.env.port || '8080');
 app.listen(port,function() {
   console.log("Server running at http://localhost:"+port);
 });
+
