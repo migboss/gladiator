c5cb034bf7b36a5f4b5c07db68b7da6c6f0fc8b0
mig <114227605+migboss@users.noreply.github.com>
2023-02-07 10:45:27 +0000

    Fix Identation of files

--- models/cardsModel.js
+++ models/cardsModel.js
@@ -1,54 +1,54 @@
 const pool = require("../config/database");
 
-          function cardFromDB(dbObj) {
-              return new Card(dbObj.crd_id,dbObj.crd_name, 
-                  dbObj.crd_img_url, dbObj.crd_lore, dbObj.crd_description,
-                  dbObj.crd_level, dbObj.crd_cost, dbObj.crd_timeout,
-                  dbObj.crd_max_usage, dbObj.crd_type);    
-          }
-          class Card {
-              constructor(id,name,url,lore,description, level, 
-                          cost, timeout, maxUsage, type) {
-                  this.id = id;
-                  this.name = name;
-                  this.url = url;
-                  this.lore = lore;
-                  this.description = description;
-                  this.level = level;
-                  this.cost = cost;
-                  this.timeout = timeout;
-                  this.maxUsage = maxUsage;
-                  this.type = type;
-              }
-          
-              static async getAll() {
-                  try {
-                      let result = [];
-                      let [dbCards,fields] = await pool.query("Select * from cards");
-                      for(let dbCard of dbCards ){
-                          result.push(cardFromDB(dbCard));
-                      }
-                      return {status: 200, result: result};
-                  } catch (err) {
-                      console.log(err);
-                      return {status: 500, result: err };
-                  }
-              }          
+function cardFromDB(dbObj) {
+    return new Card(dbObj.crd_id, dbObj.crd_name,
+        dbObj.crd_img_url, dbObj.crd_lore, dbObj.crd_description,
+        dbObj.crd_level, dbObj.crd_cost, dbObj.crd_timeout,
+        dbObj.crd_max_usage, dbObj.crd_type);
+}
+class Card {
+    constructor(id, name, url, lore, description, level,
+        cost, timeout, maxUsage, type) {
+        this.id = id;
+        this.name = name;
+        this.url = url;
+        this.lore = lore;
+        this.description = description;
+        this.level = level;
+        this.cost = cost;
+        this.timeout = timeout;
+        this.maxUsage = maxUsage;
+        this.type = type;
+    }
 
-            static async getById(id) {
-                try {
-                    let [dbCards,fields] = 
-                        await pool.query("Select * from cards where crd_id=?",[id]);
-                    if (!dbCards)
-                        return {status:404, result: {msg: "No card found with that identifier"}};
-                    let dbCard = dbCards[0];
-                    let result = cardFromDB(dbCard);
-                    return {status: 200, result: result};
-                } catch (err) {
-                    console.log(err);
-                    return {status: 500, result: err };
-                }
+    static async getAll() {
+        try {
+            let result = [];
+            let [dbCards, fields] = await pool.query("Select * from cards");
+            for (let dbCard of dbCards) {
+                result.push(cardFromDB(dbCard));
             }
-          
-          module.exports = Card;
-          
+            return { status: 200, result: result };
+        } catch (err) {
+            console.log(err);
+            return { status: 500, result: err };
+        }
+    }
+
+    static async getById(id) {
+        try {
+            let [dbCards, fields] =
+                await pool.query("Select * from cards where crd_id=?", [id]);
+            if (!dbCards)
+                return { status: 404, result: { msg: "No card found with that identifier" } };
+            let dbCard = dbCards[0];
+            let result = cardFromDB(dbCard);
+            return { status: 200, result: result };
+        } catch (err) {
+            console.log(err);
+            return { status: 500, result: err };
+        }
+    }
+}
+
+module.exports = Card;
